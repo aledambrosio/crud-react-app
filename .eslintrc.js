14d85169a4b9c0045e7d81833cc3b7f43a7ef1be
Alejandro D'Ambrosio <alejandro.dambrosio@gmail.com>
2021-10-08 15:17:22 -0300

    Nivelada config eslint y prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'google'],
+  extends: ['plugin:react/recommended', 'plugin:react/jsx-runtime', 'google'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -11,11 +11,22 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['react',
-    'prettier'],
-  rules: {},
-  overrides: [{
-    files: ['.eslintrc.js'],
-    env: {node: true},
-  }],
-};
+  settings: {
+    react: { version: 'detect' },
+  },
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'object-curly-spacing': 'off',
+    'array-bracket-spacing': 'off',
+    'computed-property-spacing': 'off',
+    'semi': 'off',
+    'indent': ['error', 2],
+  },
+  overrides: [
+    {
+      files: ['.eslintrc.js'],
+      env: { node: true },
+    },
+  ],
+}
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  bracketSpacing: true,
+}
